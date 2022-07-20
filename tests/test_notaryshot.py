74b0ff2b116be8390836b07b973bb6eaf6e00c05
Evgeny Tsvigun <utgarda@gmail.com>
2022-07-20 03:12:49 +0300

    Issue #1 test mint implementation with oracle data

--- tests/test_notaryshot.py
+++ tests/test_notaryshot.py
@@ -1,5 +1,8 @@
 import pytest, brownie
 
+ORACLE_CONTRACT = "0x4244456D81DeFE91a00d735fA57A699FdaAd747e"
+JOB_ID = "766defa685f8407faeda40a3784932f5"
+
 
 @pytest.fixture(scope='module')
 def minter(accounts):
@@ -8,7 +11,9 @@ def minter(accounts):
 
 @pytest.fixture
 def notaryshot(NotaryShot, dev):
-    return dev.deploy(NotaryShot)
+    return dev.deploy(ORACLE_CONTRACT,
+                      JOB_ID,
+                      NotaryShot)
 
 
 def test_symbol(notaryshot):
