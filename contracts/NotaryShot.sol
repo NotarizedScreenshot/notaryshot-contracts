e915cf9c9a788836c75a22041673e56850a2f1e6
Evgeny Tsvigun <utgarda@gmail.com>
2022-07-21 21:00:36 +0300

    Issue #1 events logged, including MintRequestRefused, parallel requests processed

--- contracts/NotaryShot.sol
+++ contracts/NotaryShot.sol
@@ -1,22 +1,29 @@
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/token/ERC721/ERC721.sol";
+import "@openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
 import "../interfaces/INotaryShot.sol";
 import '@chainlink/ChainlinkClient.sol';
 
 
-contract NotaryShot is ERC721, INotaryShot, ChainlinkClient {
+contract NotaryShot is ERC721Enumerable, INotaryShot, ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
-    event RequestContentHashFulfilled(string url, uint256 sha256sum);
+    event RequestContentHashSent(address indexed requester, string url, uint256 sha256sum);
+    event RequestContentHashFulfilled(address indexed requester, string url, uint256 sha256sum);
+    event MintRequestRefused(address indexed requester, string url, uint256 expectedSha256sum, uint256 actualSha256sum);
+
+    struct RequestData {
+        address requester;
+        string url;
+        uint256 sha256sum;
+    }
 
     uint256 private constant ORACLE_PAYMENT = 10 ** 15;
     address public oracle;
     bytes32 public jobId;
-    //    mapping(bytes32 => address) //TODO keep user address and submitted hash
-    address minter;               //TODO
-    string url;                   //TODO
-    uint256 urlContentHash;       //TODO
+
+    mapping(bytes32 => RequestData) public requestIdToAddress;
+
 
     constructor(address _oracle, string memory _jobid) ERC721("NotarizedScreenshot", "NS"){
         setChainlinkToken(0xb0897686c545045aFc77CF20eC7A532E3120E0F1);
@@ -24,36 +31,41 @@ contract NotaryShot is ERC721, INotaryShot, ChainlinkClient {
         jobId = stringToBytes32(_jobid);
     }
 
-    function setJobId(string memory _jobid) external { //TODO permissions
+    function setJobId(string memory _jobid) external {//TODO permissions
         jobId = stringToBytes32(_jobid);
     }
 
     function submitMint(string calldata _url, uint256 _urlContentHash) external {
-        minter = msg.sender;
-        url = _url;
-        urlContentHash = _urlContentHash;
-        requestContentHash(_url);
+        bytes32 requestId = requestContentHash(_url);
+        emit RequestContentHashSent(msg.sender, _url, _urlContentHash);
+        requestIdToAddress[requestId].requester = msg.sender;
+        requestIdToAddress[requestId].url = _url;
+        requestIdToAddress[requestId].sha256sum = _urlContentHash;
     }
 
     function tokenURI(uint256 tokenId) public view override(ERC721, IERC721Metadata) returns (string memory) {
         return "https://ytwfrmzfwi7oigsoinjcijvyn4kxg4x2z2zxqts5qv3zdapqtn6a.arweave.net/xOxYsyWyPuQaTkNSJCa4bxVzcvrOs3hOXYV3kYHwm3w";
     }
 
-    function requestContentHash(string calldata _url) private {
+    function requestContentHash(string calldata _url) private returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(
             jobId,
             address(this),
             this.fulfillContentHash.selector
         );
         req.add('get', _url);
-        sendChainlinkRequestTo(oracle, req, ORACLE_PAYMENT);
+        requestId = sendChainlinkRequestTo(oracle, req, ORACLE_PAYMENT);
     }
 
     function fulfillContentHash(bytes32 _requestId, uint256 _sha256sum) public recordChainlinkFulfillment(_requestId) {
-        emit RequestContentHashFulfilled(url, _sha256sum);
-        if (_sha256sum == urlContentHash) {
-            _mint(minter, urlContentHash);
+        RequestData memory requestData = requestIdToAddress[_requestId];
+        emit RequestContentHashFulfilled(msg.sender, requestData.url, _sha256sum);
+        if (_sha256sum == requestData.sha256sum) {
+            _mint(requestData.requester, _sha256sum);
+        } else {
+            emit MintRequestRefused(msg.sender, requestData.url, requestData.sha256sum, _sha256sum);
         }
+        delete requestIdToAddress[_requestId];
     }
 
     function stringToBytes32(string memory source) private pure returns (bytes32 result) {
--- scripts/deploy_notaryshot.py
+++ scripts/deploy_notaryshot.py
@@ -1,12 +1,11 @@
 from brownie import NotaryShot, accounts, interface
-from pathlib import Path
 
 PUBLISH_SOURCE = True
 
 # oracle contract address, job id without dashes
 # 0x4244456D81DeFE91a00d735fA57A699FdaAd747e,766defa685f8407faeda40a3784932f5
 ORACLE_CONTRACT = "0x4244456D81DeFE91a00d735fA57A699FdaAd747e"
-JOB_ID = "766defa685f8407faeda40a3784932f5"
+JOB_ID = "f1b0d529d91a49bc92c1b937a4b6f641"
 
 
 # export POLYGONSCAN_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXX___FIXME
@@ -20,11 +19,3 @@ def main():
         {'from': deployer},
         publish_source=PUBLISH_SOURCE,
     )
-
-    link = interface.ERC20("0xb0897686c545045aFc77CF20eC7A532E3120E0F1")
-    link.transferFrom(deployer, notaryshot, 10 ** 16, {'from': deployer})
-
-    notaryshot.submitMint("https://i.kym-cdn.com/entries/icons/original/000/018/012/this_is_fine.jpeg",
-                          "0xa899e6cd5d2be4dd502fa19684cc2fdcbf1d2826bc93de4f6a9420848eaf5c29",
-                          {'from': deployer},
-                          )
