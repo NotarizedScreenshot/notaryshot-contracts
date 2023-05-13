e6235781b9f2c0f05d6e9ac8f2233559edd7f94e
etsvigun <utgarda@gmail.com>
2023-05-13 14:32:53 +0300

    NotaryShot: fix the only helloworld test #8

--- tests/test_notaryshot.py
+++ tests/test_notaryshot.py
@@ -2,6 +2,7 @@ import pytest, brownie
 
 ORACLE_CONTRACT = "0x4244456D81DeFE91a00d735fA57A699FdaAd747e"
 JOB_ID = "766defa685f8407faeda40a3784932f5"
+LINK_TOKEN = "0xb0897686c545045aFc77CF20eC7A532E3120E0F1"
 
 
 @pytest.fixture(scope='module')
@@ -11,9 +12,14 @@ def minter(accounts):
 
 @pytest.fixture
 def notaryshot(NotaryShot, dev):
-    return dev.deploy(ORACLE_CONTRACT,
-                      JOB_ID,
-                      NotaryShot)
+    return NotaryShot.deploy(
+        LINK_TOKEN,
+        ORACLE_CONTRACT,
+        JOB_ID,
+        "TestNotaryShot",
+        "NS",
+        {'from': dev}
+    )
 
 
 def test_symbol(notaryshot):
